8b838c6ef689f84da02e98ed0e25710545e289b3
BayanBushnaq <bayanmohdali@yahoo.com>
2023-01-21 15:38:18 +0000

    FrontEnd design

--- src/screens/CompareCandidatesPage.js
+++ src/screens/CompareCandidatesPage.js
@@ -1,5 +1,14 @@
 import React from "react";
-
+import ClientPreview from "../components/ClientPreview/ClientPreview.js";
+import './test.css'
 export const CompareCandidatesPage = () => {
-  return <div>CompareCandidatesPage</div>;
+  return (
+    <div style={{display:'flex'}}>
+    <div className="Box" >
+    <div  ><ClientPreview/></div>
+    <div ><ClientPreview/></div>
+    </div>
+    </div>
+  )
+ 
 };
