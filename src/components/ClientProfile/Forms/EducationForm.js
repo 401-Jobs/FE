f2711aec96991b11d7e40101931dcfb32e1c9c97
islam <islamalghoul1997@gmail.com>
2023-01-22 08:42:56 -0800

    feat: adding defaluts to the forms

--- src/components/ClientProfile/Forms/EducationForm.js
+++ src/components/ClientProfile/Forms/EducationForm.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../context/auth';
+import { JoobSeekerContext } from '../../../context/joobseeker';
 const EducationForm = () => {
+  const { userEducation, updateJobseekerContact } = useContext(JoobSeekerContext);
+  const { token } = useContext(AuthContext);
+  let {institute,degree,major,start,end}=userEducation[0]
+
+
+  let handelSubmit=(e)=>{
+    e.preventDefault()
+
+  }
   return (
-    <Form>
+    <Form onSubmit={handelSubmit}>
         <div className='form_div'>
         <div className='name_div'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Institute</Form.Label>
-        <Form.Control type="text" placeholder="Enter institute name" />
+        <Form.Control type="text" placeholder="Enter institute name" defaultValue={institute} name="institute" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Degree</Form.Label>
-        <Form.Control type="text" placeholder="Enter your degree" />
+        <Form.Control type="text" placeholder="Enter your degree" defaultValue={degree} name="degree" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Major</Form.Label>
-        <Form.Control type="text" placeholder="Enter your major" />
+        <Form.Control type="text" placeholder="Enter your major" defaultValue={major} name="major"/>
       </Form.Group>
         </div>
         <div className='name_div'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Start</Form.Label>
-        <Form.Control type="date" placeholder="Start year" />
+        <Form.Control type="date" placeholder="Start year" defaultValue={start} name="start"/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>End</Form.Label>
-        <Form.Control type="date" placeholder="Graduation year" />
+        <Form.Control type="date" placeholder="Graduation year" defaultValue={end} name="end"/>
       </Form.Group>
         </div>
       
--- src/components/ClientProfile/Forms/WebsitesForm.js
+++ src/components/ClientProfile/Forms/WebsitesForm.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../context/auth';
+import { JoobSeekerContext } from '../../../context/joobseeker';
 const WebsitesForm = () => {
+  const { userDetails, updateJobseekerContact } = useContext(JoobSeekerContext);
+  const { token } = useContext(AuthContext);
+  let {linkedin,github,porto}=userDetails
   return (
     <Form>
     <div className='form_div'>
     <div style={{width:"80%", margin:"auto"}}>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Websites</Form.Label>
-    <Form.Control type='text' placeholder="Add your social link" />
+    <Form.Control type='text' placeholder="Add your linkedin link" name="linkedin" defaultValue={linkedin} />
+    <Form.Control type='text' placeholder="Add your github link" name="github" defaultValue={github} />
+    <Form.Control type='text' placeholder="Add your portofolio link" name="porto" defaultValue={porto} />
+
   </Form.Group>
     </div>
   </div>
