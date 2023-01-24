df67f9561034272277fcf590e85590c739cdbdc1
islam <islamalghoul1997@gmail.com>
2023-01-24 01:01:43 -0800

    feat:adding functionality to the candidates profile

--- src/components/ClientPreviews/Skills.js
+++ src/components/ClientPreviews/Skills.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {client_prev_data} from './client_prev_data.js'
-const Skills = () => {
+const Skills = (props) => {
+  let skills=String(props.skills)
+    skills=skills.split(",") 
+ 
+    
   return (
     <div>
         <p style={{fontSize:'x-large'}}>Skills</p>
         <ul style={{display:'flex',flexWrap:'wrap',gap:'10px'}}>
-            {client_prev_data[0]['skills'].map(item=>{
+            {skills.map(item=>{
                 return (
                     <li>{item}</li>
                 )
