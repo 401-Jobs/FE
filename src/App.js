b07209f231aeed5268e33be3afe826ad29a924ba
mohammadalfayoume <mabed3819@gmail.com>
2023-01-24 08:59:50 +0300

    feat: completed search and filter candidates with design and functionality with dummy data and create an interview modal

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ClientProfilePage from "./screens/ClientProfilePage";
 import ClientPreviews from "./components/ClientPreviews/ClientPreviews";
 
 import Candidates from "./components/Candidates/Candidates";
+import CandidateSearch from "./components/CandidateSearch/CandidateSearch";
 
 import CompanyDetails from "./components/company/Infromations/CompanyDetails";
 import DashboardCompany from "./components/company/Dashboard/DashboardCompany";
@@ -87,6 +88,7 @@ function App() {
           />
 
           <Route exact path="/candidates" element={<Candidates />} />
+          <Route exact path="/candidate" element={<CandidateSearch />} /> // added by mohammad
 
           {/* /************************* END JOB SEEKER ****************************************/}
         </Routes>
---
+++ src/components/CandidateSearch/data.js
@@ -0,0 +1,47 @@
+export const data=[
+    {
+        _id:'1',
+        name:'Mohammad Alfayoume',
+        img:require('./moh.jpg'),
+        jobTitle:'Mechanical Engineering',
+        yearsOfExp:'2',
+        age:'25',
+        skills:['html','css','js']
+    },
+    {
+        _id:'2',
+        name:'Ahmad Alfayoume',
+        img:require('./moh.jpg'),
+        jobTitle:'Civil engineering',
+        yearsOfExp:'5',
+        age:'17',
+        skills:['pro','revit','autoCad']
+    },
+    {
+        _id:'3',
+        name:'Islam',
+        img:require('./moh.jpg'),
+        jobTitle:'Electrical engineering',
+        yearsOfExp:'7',
+        age:'30',
+        skills:['node','express','mongo']
+    },
+    {
+        _id:'4',
+        name:'Abed Alasal',
+        img:require('./moh.jpg'),
+        jobTitle:'Software Developer',
+        yearsOfExp:'2',
+        age:'22',
+        skills:['SQL','React','Django']
+    },
+    {
+        _id:'5',
+        name:'Bayan Bushnaq',
+        img:require('./moh.jpg'),
+        jobTitle:'Software Engineer',
+        yearsOfExp:'2',
+        age:'29',
+        skills:['SQL','React','Django']
+    },
+]
