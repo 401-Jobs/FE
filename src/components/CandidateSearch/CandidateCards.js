13bc2ae9d8fa8e2d608e609a834e46c50dbaf27d
alaa-adoos <alaaabualadoos@gmail.com>
2023-01-25 00:26:18 +0000

    candidate cards and filter design

--- src/components/CandidateSearch/CandidateCards.js
+++ src/components/CandidateSearch/CandidateCards.js
@@ -1,8 +1,12 @@
 import React,{useState,useEffect} from 'react'
-import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useNavigate } from 'react-router-dom';
 import { data } from './data';
-
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card from 'react-bootstrap/Card';
+import '../Candidates/Candidates.css'
+import Row from 'react-bootstrap/Row';
+import { Grid } from 'react-bootstrap';
 const CandidateCards = (props) => {
   const navegate= useNavigate()
   const clickHandler=()=>{
@@ -14,28 +18,38 @@ const CandidateCards = (props) => {
   return (
     
     <>
+    
+    <CardGroup>
+  
+   
+    <div style={{ width: "80%" ,"background-color":"white", 'marginTop':'75px', }} className="card testimonial-card item" >
+      <div class="card-up aqua-gradient " ></div>
       
-  <section class="mx-auto my-5">
-      
-    <div class="card testimonial-card mt-2 mb-3" style={{maxWidth:'23rem'}}>
-      <div class="card-up aqua-gradient"></div>
-      <div class="avatar mx-auto white">
-        <img src={`${process.env.REACT_APP_BACKEND_URL}${props.img}`} class="rounded-circle img-fluid"
-          alt="woman avatar"/>
+      <div class="avatar mx-auto white ">
+        <img 
+         variant="top"
+        src={`${process.env.REACT_APP_BACKEND_URL}${props.img}`}
+         className="size"
+         style={{ width: "18rem" ,"background-color":"#FF99D7" }} 
+          alt=" avatar"/>
       </div>
-      <div class="card-body text-center">
+      <div className="card-body text-cente " style={{display:'flex','flex-direction': 'column'}}>
         <h4 class="card-title font-weight-bold">{props.firstName} {props.lastName}</h4>
         <hr/>
         <p style={{fontSize:'large'}}>{props.jopTitle}</p>
         <p>Years of Experience: {props.ex}</p>
         <p>Age: {props.age} years old</p>
         <p>Skills: {props.skills}</p>
-        <button onClick={clickHandler} style={{padding:'10px 50px',backgroundColor:'#d4ccec',border:'none',borderRadius:'10px'}}>View Profile</button>
+        
+        <button class="align-self-end" onClick={clickHandler} style={{padding:'10px 50px',backgroundColor:'#d4ccec',border:'none',borderRadius:'10px',marginTop:'auto',marginRight:'50px',}}>View Profile</button>
+      
       </div>
+   
     </div>
     
-  </section>
-
+   
+   </CardGroup>
+  
 </>
 
   )
--- src/components/CandidateSearch/Filter.js
+++ src/components/CandidateSearch/Filter.js
@@ -1,17 +1,30 @@
 import React,{useState} from 'react'
 
 const Filter = (props) => {
-    
+  const[showcollapse,setShowCollapse]=useState(true) 
   return (
     <div>
-      <form onSubmit={props.submitFilterHandler}>
-        <div style={{display:'flex',flexDirection:'column',gap:'10px',marginTop:'15px'}}>
+       <button style={{}}onClick={()=>setShowCollapse(!showcollapse)} className="btn filter">
+   Filter
+   <svg class="scd" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sliders-v" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M112 96H96V16c0-8.8-7.2-16-16-16H48c-8.8 0-16 7.2-16 16v80H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h16v336c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V160h16c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm320 128h-16V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v208h-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h16v208c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V288h16c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM272 352h-16V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v336h-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h16v80c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-80h16c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16z"></path></svg>
+  </button>
+      <form onSubmit={props.submitFilterHandler} className={showcollapse?'collapse.show':'collapse'}>
+       
+       
+        
         <p style={{fontSize:'x-large',textAlign:'center'}}>Filter Candidates By</p>
-        <div style={{ display: 'flex', justifyContent: 'center', gap: '100px', marginTop:'15px'}}>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '5px', marginTop:'15px'}}>
           {/* JobTitle */}
+          <div className='labelInputMaxMin'>
           <div className='labelInput'>
             <label>Job Title</label>
             <input type='text' name='jobTitle' placeholder='job title'/>
+          
+          </div>
+          <div className='labelInput'>
+          <label>Skills</label>
+            <input type='text' name='skills' onChange={props.skillHandler} placeholder='skill'/>
+            </div>
           </div>
           {/* end JobTitle */}
           {/* exp container */}
@@ -38,13 +51,12 @@ const Filter = (props) => {
             </div>
           </div>
           {/* end age */}
+        
+
+            <div  className='labelInputMaxMin'>
+            <button onClick={props.submitHandler} style={{backgroundColor:'#58629b',padding:'5px 5px',border:'none',borderRadius:'15px',marginTop:'15px'}}>Add</button>
+        <button style={{backgroundColor:'#58629b',padding:'5px 5px',border:'none',borderRadius:'15px',marginTop:'15px'}}>Search</button>
         </div>
-        <div className='skill_container'>
-            <label>Skills</label>
-            <input type='text' name='skills' onChange={props.skillHandler} placeholder='skill'/>
-            <button onClick={props.submitHandler} style={{backgroundColor:'#58629b',padding:'5px 10px',border:'none',borderRadius:'15px'}}>Add</button>
-        <button style={{backgroundColor:'#58629b',padding:'5px 10px',border:'none',borderRadius:'15px',marginLeft:'20px'}}>Search</button>
-        </div> 
         
         <div className='' style={{display:'flex',flexWrap:'wrap',maxWidth:'600px',margin:'auto',justifyContent:'center',alignItems:'center'}}>
             <ul id="myUL" className="skillsList"></ul>
